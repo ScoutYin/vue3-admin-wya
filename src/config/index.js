e68441e61749bf892b7873d5015a88c1b4138aaa
ScoutYin <scoutyin.1900@gmail.com>
2022-08-22 12:27:58 +0800

    refactor: setupConfig

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,19 @@
+import * as WYA_VC from '@wya/vc';
+import * as WYA_VCC from '@wya/vcc';
+import { Global } from './global';
+import { getVcConfig } from './vc';
+import { getVccConfig } from './vcc';
+
 export * from './global';
 export * from './network';
 export * from './vc';
 export * from './vcc';
+
+export const setupConfig = (app, options) => {
+	// 全局信息
+	app.use(Global, options);
+	// VC配置
+	app.use(WYA_VC, getVcConfig(options));
+	// VCC配置
+	app.use(WYA_VCC, getVccConfig(options));
+};
--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import './assets/style/index.scss';
 import '@wya/vc/lib/vc.min.css';
-
 import { createApp } from 'vue';
-import * as WYA_VC from '@wya/vc';
-import * as WYA_VCC from '@wya/vcc';
-import { Global, getVcConfig, getVccConfig } from '@/config';
+import { setupConfig } from '@/config';
 import { router, setupRouter } from './router';
-
 import App from './app.vue';
 
 const app = createApp(App);
-const options = {
-	Router: router,
-};
 
-// 全局信息
-app.use(Global, options);
+setupConfig(app, { Router: router });
 setupRouter(app);
-
-// VC配置
-app.use(WYA_VC, getVcConfig(options));
-app.use(WYA_VCC, getVccConfig(options));
 
 app.mount('#app');
 

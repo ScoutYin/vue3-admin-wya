647adf3930c513776a0e992e838d61684f1724fa
ScoutYin <scoutyin.1900@gmail.com>
2022-08-24 19:55:36 +0800

    fix: vue3不支持IE(不支持原生module),故不需要legacy

--- build/vite/plugins/index.js
+++ build/vite/plugins/index.js
@@ -1,23 +1,17 @@
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import legacy from '@vitejs/plugin-legacy';
 import visualizer from './visualizer';
 import { isBuild } from '../../utils';
 
 export const createPlugins = (options) => {
 	const COMMAND_IS_BUILD = isBuild(options.command);
 
-	const plugins = [
-		vue(),
-		vueJsx()
-	];
-	
+	const plugins = [vue(), vueJsx()];
+
 	if (COMMAND_IS_BUILD) {
 		plugins.push(
 			// 包文件量分析
-			visualizer(),
-			// 兼容低版本
-			legacy()
+			visualizer()
 		);
 	}
 	return plugins;
